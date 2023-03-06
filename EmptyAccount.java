e5ac036c4775862aa0cc64cfd3fcef3b13100e04
prithik <pkarthikeyanmano937@student.fuhsd.org>
2023-03-05 22:26:10 -0800

    my code

---
+++ EmptyAccount.java
@@ -0,0 +1,21 @@
+public class EmptyAccount {
+
+public static void main(String[]args) {
+double amountInaccount;
+double amountDue;
+
+amountInaccount = 1000.00;
+amountInaccount = amountInaccount - 1500.00; 
+amountDue = 1000;
+
+
+System.out.print("You have a due of $");
+System.out.print(amountInaccount + amountDue);
+System.out.print("in your account, kindly pay before the end of the month. ");
+
+}
+
+
+
+
+}
